5aff338546813cc2dbbd273716a19665a2b93cbf
anusham <anusham@unicourt.com>
2020-09-26 17:40:01 +0530

    initial 1

---
+++ docker_test/add_number.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    # Use a breakpoint in the code line below to debug your script.
+    return a+b  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    a = 5
+    b = 3
+    res = add(a, b)
+    print"The sum is ", res
